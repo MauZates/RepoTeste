a06ac5e33d519297218854b17be61e44031efee5
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-01-09 19:47:54 -0300

    Adicionado Projeto Construtores

---
+++ Construtores/ConstrutoresAula01/Produto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ConstrutoresAula01
+{
+	internal class Produto
+	{
+		private string _nome;
+		private double _preco;
+		private int _quantidade;
+
+
+		public Produto() { }
+
+		public Produto(string nome, double preco, int quantidade)
+		{
+			_nome = nome;
+			_preco = preco;
+			_quantidade = quantidade;
+		}
+
+		public string Nome
+		{
+			get { return _nome; }
+			set {
+				if (value != null && value.Length > 1)
+				{
+					_nome = value;
+				}
+			}
+		}
+
+		public double ValorTotalEmEstoque()
+		{
+			return _preco * _quantidade;
+		}
+
+		public void AdicionarProdutos(int quantidade)
+		{
+			_quantidade += quantidade;
+		}
+
+		public void RemoverProdutos(int quantidade)
+		{
+			_quantidade -= quantidade;
+		}
+
+		public override string ToString()
+		{
+			return _nome
+			+ ", $ "
+			+ _preco.ToString("F2", CultureInfo.InvariantCulture)
+			+ ", "
+			+ _quantidade
+			+ " unidades, Total: $ "
+			+ ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+		}
+	}
+}
---
+++ Construtores/ConstrutoresAula01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConstrutoresAula01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Padrao
+			//Console.WriteLine("Entre os dados do produto:");
+			//Console.Write("Nome: ");
+			//string Nome = Console.ReadLine();
+			//Console.Write("Preço: ");
+			//double Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			//Console.Write("Quantidade no estoque: ");
+			//int Quantidade = int.Parse(Console.ReadLine());
+
+			//Produto produto = new Produto(Nome, Preco, Quantidade);
+
+			//Console.WriteLine();
+			//Console.WriteLine("Dados do produto: " + produto);
+
+			//Console.WriteLine();
+			//Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+			//int qte = int.Parse(Console.ReadLine());
+			//produto.AdicionarProdutos(qte);
+
+			//Console.WriteLine();
+			//Console.WriteLine("Dados atualizados: " + produto);
+
+			//Console.WriteLine();
+			//Console.Write("Digite o número de produtos a ser removido do estoque: ");
+			//qte = int.Parse(Console.ReadLine());
+			//produto.RemoverProdutos(qte);
+
+			//Console.WriteLine();
+			//Console.WriteLine("Dados atualizados: " + produto);
+			#endregion
+
+			Produto p = new Produto("TV", 500.00, 10);
+
+			p.SetNome("TV 4k");
+
+			Console.WriteLine(p.GetNome());
+
+		}
+	}
+}
