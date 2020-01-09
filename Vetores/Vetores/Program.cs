978f58baea4ccfa2c60e0f45caf2732b2ae41957
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-01-08 23:18:50 -0300

    Novo Programa de Vetores, Testando o GitHub

---
+++ Vetores/Vetores/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Vetores
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			double[] vect = new double[n];
+			double sum = 0.0;
+
+			for (int i = 0; i < n; i++)
+			{
+				vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+				sum += vect[i];
+			}
+
+			double avg = sum / n;
+			Console.WriteLine(avg.ToString("F2", CultureInfo.InvariantCulture));
+		}
+	}
+}
