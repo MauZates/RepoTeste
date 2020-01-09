3ecb1f959674a94f3e74205b389f874d6bd83914
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-01-09 20:30:15 -0300

    test

--- ExercicioEncapsulamento/ExercicioEncapsulamento/Program.cs
+++ ExercicioEncapsulamento/ExercicioEncapsulamento/Program.cs
@@ -8,6 +8,12 @@ namespace ExercicioEncapsulamento
 		static void Main(string[] args)
 		{
 			#region FormaMau
+
+
+
+
+
+
 			//Console.Write("Entre com o número da Conta: ");
 			//int conta = int.Parse(Console.ReadLine());
 			//Console.Write("Entre com o titular da Conta: ");
