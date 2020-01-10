bbf756724783e5f294370abd1e7193a01ef0db5a
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-01-09 21:38:30 -0300

    Vetores2

---
+++ Vetores2/Vetores2/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Vetores2
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Vetores2/Vetores2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+namespace Vetores2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Product[] products = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                products[i] = new Product
+                {
+                    Name = name,
+                    Price = price
+                };
+            }
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += products[i].Price;
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("AVERAGE " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
