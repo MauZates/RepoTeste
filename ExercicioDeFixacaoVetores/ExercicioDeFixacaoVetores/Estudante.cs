9783c125f057ae3d0257ffb30e619080e754713f
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-01-12 21:28:13 -0300

    Novo Exercicio finalizado

---
+++ ExercicioDeFixacaoVetores/ExercicioDeFixacaoVetores/Estudante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioDeFixacaoVetores
+{
+    public class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ ExercicioDeFixacaoVetores/ExercicioDeFixacaoVetores/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacaoVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] estudantes = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel # {i}:");
+
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                int quarto = int.Parse(Console.ReadLine());
+                estudantes[quarto] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (estudantes[i] != null)
+                {
+                    Console.WriteLine(i + ": " + estudantes[i]);
+                }
+            }
+
+        }
+    }
+}
