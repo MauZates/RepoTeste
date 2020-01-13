123b1cc171abad954f9d902102e9b82cfe171640
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-01-12 22:17:52 -0300

    Exercicio 1 Finalizado

---
+++ ExeVetor01/ExeVetor01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExeVetor01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Salarios = new int[10];
+            int n = 10;
+            int MaiorSalario = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Digite o {i} salário: ");
+                int salario = int.Parse(Console.ReadLine());
+                Salarios[i] = salario;
+
+                if (Salarios[i] > MaiorSalario)
+                {
+                    MaiorSalario = Salarios[i];
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("O maior salario é o " + MaiorSalario);
+        }
+    }
+}
