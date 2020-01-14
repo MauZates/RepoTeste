3dbb99e43f76031300833245f440f90e71491f9b
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-01-13 21:46:15 -0300

    Aprendido como Fazer um (Params,Ref,Out).

---
+++ Params/Params/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParamsAndRefOut
+{
+    class Calculator
+    {
+        #region Params
+
+        //public static int Sum(params int[] numbers)
+        //{
+        //    int sum = 0;
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        sum += numbers[i];
+        //    }
+        //    return sum;
+        //}
+
+        #endregion
+        #region Ref
+        //public static void Triple(ref int x)
+        //{
+        //    x = x * 3;
+        //}
+        #endregion
+
+        #region Out
+
+        public static void Triple(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+
+        #endregion
+    }
+}
---
+++ Params/Params/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ParamsAndRefOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Params
+
+            //int s1 = Calculator.Sum(1, 3);
+            //int s2 = Calculator.Sum(1, 3, 33, 66, 3);
+            //Console.WriteLine(s1);
+            //Console.WriteLine(s2);
+
+            #endregion
+
+            #region Ref
+
+            //int a = 10;
+            //Calculator.Triple(ref a);
+            //Console.WriteLine(a);
+
+            #endregion
+
+            #region Out
+
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+
+            #endregion
+        }
+    }
+}
