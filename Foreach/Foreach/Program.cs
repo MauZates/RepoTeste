b13d7637a08076009c53b7360a7ac6ccc3c94f27
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-01-20 21:37:26 -0300

    Foreach

---
+++ Foreach/Foreach/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[] { " Maria ", " Jose ", " Souza " };
+            foreach (var nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
