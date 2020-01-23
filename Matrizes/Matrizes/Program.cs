26478d6bf359f3ee4824d6f28e2a03077fa80a9d
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-01-23 20:11:29 -0300

    Matrizes

---
+++ Matrizes/Matrizes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[5, 5];
+
+            Console.WriteLine(mat.Length); // Tamanho total 5 * 5 = 25
+            Console.WriteLine(mat.Rank); // primeira dimensao da matriz, quantidade de linhas
+            Console.WriteLine(mat.GetLength(0)); //dimensao 0 da matriz tem tamanho 5
+            Console.WriteLine(mat.GetLength(1)); //dimensao 1 da matriz tem tamanho 5
+
+        }
+    }
+}
