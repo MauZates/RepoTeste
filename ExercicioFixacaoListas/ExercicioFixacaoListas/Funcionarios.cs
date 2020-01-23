fd94b3d89f539956b563676d93a6775bc4f47143
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-01-22 21:15:38 -0300

    ExercicioFixacaoListas RESOLVIDO!

---
+++ ExercicioFixacaoListas/ExercicioFixacaoListas/Funcionarios.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacaoListas
+{
+    /// <summary>
+    /// Classe interna de Funcionarios
+    /// </summary>
+    internal class Funcionarios
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        /// <summary>
+        /// Construtor Padrao
+        /// </summary>
+        public Funcionarios()
+        {
+
+        }
+
+        /// <summary>
+        /// Construtor dos funcionários
+        /// </summary>
+        /// <param name="id">Id dos Funcionarios</param>
+        /// <param name="nome">Nome dos Funcionarios</param>
+        /// <param name="salario">Salario Funcionarios</param>
+        public Funcionarios(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        /// <summary>
+        /// Método responsável por incrementar o salario de acordo com a
+        /// porcentagem informada
+        /// </summary>
+        /// <param name="porcentagem"> Porcentagem informada </param>
+        public void IncrementarSalario(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100.00;
+        }
+
+        /// <summary>
+        /// Retorno da mensagem por string
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Nome
+                + ", "
+                + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExercicioFixacaoListas/ExercicioFixacaoListas/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioFixacaoListas
+{
+    /// <summary>
+    /// Classe Program, onde é execultado o scopo do projeto
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Método statico onde se inicia o program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos funcionarios serão cadastrados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            List<Funcionarios> funcionarios = new List<Funcionarios>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Funcionario #" + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                funcionarios.Add(new Funcionarios(id, nome, salario));
+                Console.WriteLine("-----------------------------------");
+            }
+
+            bool exist = false;
+            while (!exist)
+            {
+                Console.Write("Digite o Id do funcionario que deseja incrementar o salario: ");
+                string idFunc = Console.ReadLine();
+                bool id = int.TryParse(idFunc, out int IdValido);
+                if (id)
+                {
+                    Funcionarios func = funcionarios.Find(x => x.Id == IdValido);
+
+                    if (func != null)
+                    {
+                        Console.Write("Digite a porcentagem que deseja aumentar: ");
+                        double _porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        func.IncrementarSalario(_porcentagem);
+                        exist = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("------------------------------------------------------------------------------------------------------");
+                        Console.WriteLine("Esse id não existe tente novamente");
+                        Console.WriteLine("------------------------------------------------------------------------------------------------------");
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("------------------------------------------------------------------------------------------------------");
+                    Console.WriteLine("Digite números!");
+                    Console.WriteLine("------------------------------------------------------------------------------------------------------");
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Atualizando a lista de funcionarios: ");
+            foreach (Funcionarios obj in funcionarios)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
