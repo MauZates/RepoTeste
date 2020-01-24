e790e2e39e9634c7dffd179120b6037c1f952682
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-01-23 21:11:50 -0300

    MatrizesExercicio

---
+++ ExercicioMatrizes/ExercicioMatrizes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExercicioMatrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++) // linhas
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++) // colunas
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Diagonal Principal");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i,i] + " ");
+            }
+
+            Console.WriteLine();
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat [i,j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quantidade de números negativos: " + count);
+
+        }
+    }
+}
