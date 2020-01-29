6f1af0134be53f034a9c0ea0370d663189df0cba
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-01-28 21:26:46 -0300

    Timespans aulas

---
+++ TimeSpan/TimeSpan/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TimeSpans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan();
+            TimeSpan t2 = new TimeSpan(900000000L);
+            TimeSpan t3 = new TimeSpan(2, 11, 21);
+            TimeSpan t4 = new TimeSpan(1, 2, 11, 21);
+            TimeSpan t5 = new TimeSpan(1, 2, 11, 21, 321);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+
+
+            TimeSpan t7 = TimeSpan.FromDays(1.5);
+            TimeSpan t8 = TimeSpan.FromHours(1.5);
+            TimeSpan t9 = TimeSpan.FromMinutes(1.5);
+            TimeSpan t10 = TimeSpan.FromSeconds(1.5);
+            TimeSpan t11 = TimeSpan.FromMilliseconds(1);
+            TimeSpan t12 = TimeSpan.FromTicks(900000000L);
+            Console.WriteLine(t7);
+            Console.WriteLine(t8);
+            Console.WriteLine(t9);
+            Console.WriteLine(t10);
+            Console.WriteLine(t11);
+            Console.WriteLine(t12);
+        }
+    }
+}
