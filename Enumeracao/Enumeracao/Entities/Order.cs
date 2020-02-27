2f88e8e9eb5d3312e6ec5bd9868919fff428321e
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-02-26 21:22:39 -0300

    Primeira aula de Enumeração

---
+++ Enumeracao/Enumeracao/Entities/Order.cs
@@ -0,0 +1,23 @@
+﻿using Enumeracao.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enumeracao.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment.ToShortDateString()
+                + ", "
+                + Status;
+        }
+    }
+}
---
+++ Enumeracao/Enumeracao/Program.cs
@@ -0,0 +1,26 @@
+﻿using Enumeracao.Entities;
+using Enumeracao.Entities.Enums;
+using System;
+namespace Enumeracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
