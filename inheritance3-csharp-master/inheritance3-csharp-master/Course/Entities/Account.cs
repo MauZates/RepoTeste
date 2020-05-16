eac4d4b21976abbc0dee516d0c2c00cfd4329445
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-05-16 15:04:31 -0300

    Classes abstratas

--- inheritance3-csharp-master/inheritance3-csharp-master/Course/Entities/Account.cs
+++ inheritance3-csharp-master/inheritance3-csharp-master/Course/Entities/Account.cs
@@ -1,6 +1,6 @@
 ﻿namespace Course.Entities
 {
-    class Account
+    abstract class Account
     {
         public int Number { get; private set; }
         public string Holder { get; private set; }
--- inheritance3-csharp-master/inheritance3-csharp-master/Course/Program.cs
+++ inheritance3-csharp-master/inheritance3-csharp-master/Course/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using Course.Entities;
+using System.Globalization;
 
 namespace Course
 {
@@ -7,14 +9,22 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Account acc1 = new Account(1001, "Alex", 500.0);
-            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
+            List<Account> list = new List<Account>
+            {
+                new SavingsAccount(1001, "Alex", 500.0, 0.01),
+                new BusinessAccount(1002, "Maria", 500.0, 400),
+                new SavingsAccount(1003, "Bob", 500.0, 0.01),
+                new BusinessAccount(1004, "Anna", 500.0, 500.0)
+            };
+            double sum = 0.0;
 
-            acc1.Withdraw(10.0);
-            acc2.Withdraw(10.0);
+            foreach (Account acc in list)
+            {
+                sum += acc.Balance;
+            }
 
-            Console.WriteLine(acc1.Balance);
-            Console.WriteLine(acc2.Balance);
+            Console.WriteLine("Total Balance: " + sum.ToString("F2",CultureInfo.InvariantCulture));
+
         }
     }
 }
