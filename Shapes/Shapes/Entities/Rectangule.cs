cf4734ca7d7e0581a4c40aac64855e73648de33a
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-05-16 16:01:01 -0300

    Shapes Exercice

---
+++ Shapes/Shapes/Entities/Rectangule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shapes.Entities.Enums;
+
+namespace Shapes.Entities
+{
+    class Rectangule : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangule(double width, double height, Color color) : base(color)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+    }
+}
