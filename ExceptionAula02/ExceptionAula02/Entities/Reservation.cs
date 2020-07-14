03aa81f3d601f8b3c505501d87b7c75f66cf1d6d
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-07-13 22:51:44 -0300

    Excessões personalizadas

---
+++ ExceptionAula02/ExceptionAula02/Entities/Reservation.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionAula02.Entities
+{
+    /// <summary>
+    /// Classe Reserva
+    /// </summary>
+    class Reservation
+    {
+        /// <summary>
+        /// Número do quarto
+        /// </summary>
+        public int RoomNumber { get; set; }
+        /// <summary>
+        /// Dia da Entrada
+        /// </summary>
+        public DateTime CheckIn { get; set; }
+        /// <summary>
+        /// Dia da Saida
+        /// </summary>
+        public DateTime CheckOut { get; set; }
+
+        /// <summary>
+        /// Construtor padrao
+        /// </summary>
+        public Reservation()
+        {
+
+        }
+        /// <summary>
+        /// Construtor com os 3 argumentos
+        /// </summary>
+        /// <param name="roomNumber"> Numero do quarto</param>
+        /// <param name="checkIn"> Dia da entrada</param>
+        /// <param name="checkOut"> Dia da saida</param>
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        /// <summary>
+        /// Método que pega a data da entrada informada, subtrai com a data de saida
+        /// e retorna os dias
+        /// </summary>
+        /// <returns>Retorna o total de dias entre o checkin e checkout</returns>
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        /// <summary>
+        /// Atualiza os dados de entrada e saida com as novas datas informadas
+        /// </summary>
+        /// <param name="checkIn">Nova data de entrada</param>
+        /// <param name="checkOut">Nova data de saida</param>
+        public string UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                return "Reservation dates for update mus be future dates";
+            }
+            else if (checkOut <= checkIn)
+            {
+                return "Error in reservation: Check-out date mmust be after check-in date";
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Formata a frase de retorno
+        /// </summary>
+        /// <returns>Retorna a frase formatada, com a informação de checkin
+        /// checkout e quandos dias de intervalo entre os dois</returns>
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
---
+++ ExceptionAula02/ExceptionAula02/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ExceptionAula02.Entities;
+
+namespace ExceptionAula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room Number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-in: ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out: ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+            
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date mmust be after check-in date");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine("Reservation: "+ reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in: ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out: ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                
+                string erro = reservation.UpdateDates(checkIn, checkOut);
+                if (erro != null)
+                {
+                    Console.WriteLine("Error in reservation: " + reservation);
+                }
+                else
+                {
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+            }
+        }
+    }
+}
