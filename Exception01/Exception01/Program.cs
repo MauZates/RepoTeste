162dcaf0f4f344e9efab76b5e09d42ebf93cc869
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-07-13 22:04:26 -0300

    Primeiro Exercicio de tratamento de exercoes

---
+++ Exception01/Exception01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exception01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divison by zero is not allowed");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("is not a number");
+            }
+        }
+    }
+}
