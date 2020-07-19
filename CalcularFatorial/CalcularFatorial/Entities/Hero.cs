43994f878e2b3c42ae712fc6617fb713f30d0ca5
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-07-19 16:52:00 -0300

    Fatorial e heroi que Voa

---
+++ CalcularFatorial/CalcularFatorial/Entities/Hero.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcularFatorial.Entities
+{
+    /// <summary>
+    /// Class hero
+    /// </summary>
+    public class Hero
+    {
+        /// <summary>
+        /// Name of Hero
+        /// </summary>
+        public string Nome { get; set; }
+        /// <summary>
+        /// Date of birthday
+        /// </summary>
+        public DateTime DataDenascimento { get; set; }
+        /// <summary>
+        /// Kryptonita level
+        /// </summary>
+        public int Kryptonita { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Hero()
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor with 3 param
+        /// </summary>
+        /// <param name="nome"> Name </param>
+        /// <param name="dataDenascimento"> Date of Birthday </param>
+        /// <param name="kryptonita"> Level </param>
+        public Hero(string nome, DateTime dataDenascimento, int kryptonita)
+        {
+            Nome = nome;
+            DataDenascimento = dataDenascimento;
+            Kryptonita = kryptonita;
+        }
+
+        /// <summary>
+        /// Fly Hero according kryptonita level
+        /// </summary>
+        /// <returns> String </returns>
+        public string Fly()
+        {
+            if (Kryptonita < 2)
+            {
+                return "Voando...";
+            }
+            return null;
+        }
+    }
+}
---
+++ CalcularFatorial/CalcularFatorial/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CalcularFatorial.Entities;
+
+namespace CalcularFatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ulong Numero;
+            //ulong Fatorial;
+            //ulong Contador;
+
+            //Console.WriteLine("Digite o número para calcular o fatorial: ");
+            //Numero = ulong.Parse(Console.ReadLine());
+
+            //Fatorial = 1;
+            //Contador = 1;
+
+            //while (Contador <= Numero)
+            //{
+            //    Fatorial *= Contador;
+            //    Contador += 1;
+            //}
+
+            //Console.WriteLine("O fatorial de " + Numero + " é " + Fatorial);
+
+            Hero hero = new Hero();
+
+            Console.WriteLine("Digite o Nome do Heroi: ");
+            hero.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a data de nascimento ex: XX/XX/XXXX");
+            hero.DataDenascimento = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o Nível de Kryptonita");
+            hero.Kryptonita = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(hero.Fly());
+
+        }
+    }
+}
