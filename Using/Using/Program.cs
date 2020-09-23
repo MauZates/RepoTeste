23cf2a26d4458267cb1685af16d180e85947cda2
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-09-22 23:20:01 -0300

    Aprendendo sobre Using

---
+++ Using/Using/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamAndStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
