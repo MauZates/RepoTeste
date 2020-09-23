089280b25d051ad7226ab60b92995fb7f4fe8223
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-09-22 22:53:36 -0300

    Trabalhando com File e FileInfo

---
+++ File/File/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                FileInfo file = new FileInfo(sourcePath);
+                file.CopyTo(targetPath);
+                string[] lines = System.IO.File.ReadAllLines(sourcePath);
+
+                foreach (var line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
