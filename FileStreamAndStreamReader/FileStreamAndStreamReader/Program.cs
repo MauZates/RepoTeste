b03bae475f9e25f57d4680fa023d0a3aeefd8b8c
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2020-09-22 23:11:14 -0300

    StreamReader e FileStream

---
+++ FileStreamAndStreamReader/FileStreamAndStreamReader/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamAndStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+            StreamReader streamReader = null;
+            try
+            {   
+                streamReader = File.OpenText(path);
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+            }
+        }
+    }
+}
