02ff5b3a9ede6ce2ada1cbe8b10ffd4c7b56cc50
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2021-01-25 17:03:38 -0300

    Contador Maior de Idade

---
+++ ProgramMaiorIdade/ProgramMaiorIdade/Class/People.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramMaiorIdade.Class
+{
+    public static class PeopleExtensions
+    {
+        public static List<People> QuantidadeMaiorIdade(this List<People> peoples, People people)
+        {
+            if (peoples.Count > 0)
+            {
+                peoples = people.QtdMaiorDeIdade(peoples);
+                return peoples;
+            }
+            return new List<People>();
+        }
+    }
+
+    public class People
+    {
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        private string _nome { get; set; }
+
+        public int Idade
+        {
+            get { return _idade; }
+            set { _idade = value; }
+        }
+
+        private int _idade { get; set; }
+
+        public int Quantidade
+        {
+            get { return _quantidade; }
+            set { _quantidade = value; }
+        }
+
+        private int _quantidade { get; set; }
+
+        /// <summary>
+        /// Contrutor Padrão
+        /// </summary>
+        public People()
+        {
+
+        }
+
+        public List<People> QtdMaiorDeIdade(List<People> peoples)
+        {
+            try
+            {
+                if (peoples.Count > 0)
+                {
+                    peoples = peoples.Where(x => x._idade >= 18).ToList();
+                    return peoples;
+                }
+                return new List<People>();
+            }
+            catch (Exception exp)
+            {
+                throw exp;
+            }
+        }
+    }
+}
---
+++ ProgramMaiorIdade/ProgramMaiorIdade/Program.cs
@@ -0,0 +1,35 @@
+﻿using ProgramMaiorIdade.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramMaiorIdade
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<People> peoples = new List<People>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                string auxNome = "";
+                Console.Write("Digite o Nome: ");
+                auxNome = Console.ReadLine();
+
+                Console.Write("Digite a Idade: ");
+                int.TryParse(Console.ReadLine(), out int auxIdade);
+
+                peoples.Add(new People() { Nome = auxNome, Idade = auxIdade });
+                Console.WriteLine();
+            }
+
+            peoples = peoples.QuantidadeMaiorIdade(new People());
+
+            Console.WriteLine("Existem " + peoples.Count + (peoples.Count == 1 ? " pessoa maior de idade" : " pessoas maiores de idade"));
+
+        }
+    }
+}
