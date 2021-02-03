c393430ef40803e454ac4ee4e8df636c61eecc3b
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2021-02-03 00:48:41 -0300

    ICompareTO, Maneiro de Usar

---
+++ InterfaceIComparable/InterfaceIComparable/Entities/Employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace InterfaceIComparable.Entities
+{
+    class Employee : IComparable
+    {
+        public string Name { get => _name; set => _name = value; }
+        private string _name { get; set; }
+
+        public double Salary { get => _salary; set => _salary = value; }
+        private double _salary { get; set; }
+
+        public Employee()
+        {
+
+        }
+
+        /// <summary>
+        /// Construtor que recebe um arquivo csv
+        /// </summary>
+        /// <param name="csvEmployee">Cada linha do arquivo csv exemplo Mary Lary,1500.00</param>
+        //public Employee(string csvEmployee)
+        //{
+        //    string[] employeeLine = csvEmployee.Split(',');
+        //    _name = employeeLine[0];
+        //    _salary = double.Parse(employeeLine[1],CultureInfo.InvariantCulture);
+        //}
+
+        //public int CompareTo(object obj)
+        //{
+        //    if (!(obj is Employee))
+        //    {
+        //        throw new ArgumentException("The object is not a Employee");
+        //    }
+
+        //    Employee temp = obj as Employee;
+
+        //    return _name.CompareTo(temp._name);
+        //}
+
+        public override string ToString()
+        {
+            return _name + " = " + _salary.ToString("F2", CultureInfo.InvariantCulture);
+        }        
+
+        public List<Employee> CarregaTudoNumeLista(string[] allFile)
+        {
+            List<Employee> allEmployee = new List<Employee>();
+            foreach (var lineFile in allFile)
+            {
+                allEmployee.Add(new Employee()
+                {
+                    _name = lineFile.Split(',')[0],
+                    _salary = double.Parse(lineFile.Split(',')[1], CultureInfo.InvariantCulture)
+                });
+            }
+            return allEmployee;
+        }
+    }
+}
---
+++ InterfaceIComparable/InterfaceIComparable/Program.cs
@@ -0,0 +1,60 @@
+﻿using InterfaceIComparable.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\in.txt";
+
+            //Forma Original
+
+            //try
+            //{
+            //    using (StreamReader sr = File.OpenText(path))
+            //    {
+            //        List<Employee> employees = new List<Employee>();
+            //        while (!sr.EndOfStream)
+            //        {
+            //            employees.Add(new Employee(sr.ReadLine()));
+            //        }
+
+            //        employees.Sort();
+
+            //        foreach (Employee employee in employees)
+            //        {
+            //            Console.WriteLine(employee);
+            //        }
+            //    }
+            //}
+            //catch (IOException e)
+            //{
+            //    Console.WriteLine("An error occured");
+            //    Console.WriteLine(e.Message);
+            //}
+
+            try
+            {
+                string[] ArquivoInteiro = File.ReadAllLines(path);
+                List<Employee> employees = new Employee().CarregaTudoNumeLista(ArquivoInteiro);
+                employees.Sort();
+                foreach (Employee employee in employees)
+                {
+                    Console.WriteLine(employee);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
