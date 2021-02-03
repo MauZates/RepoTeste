22f18e8d023bee894368ec881387db155307b75e
Mauricio Viana de Moura <mauricio.moura@modernasistemas.com.br>
2021-02-02 23:21:49 -0300

    App de Teste

---
+++ AppdeTestes/AppdeTestes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppdeTestes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inicial = "07:05";
+            string final = "07:05";
+
+            int iniTime = int.Parse(inicial.Replace(":", ""));
+            int fimTime = int.Parse(final.Replace(":", ""));
+
+            //if (iniTime.ToString().Length == 3)
+            //{
+            //    Console.WriteLine(iniTime.ToString("D4"));
+            //}
+            Console.WriteLine(iniTime);
+            Console.WriteLine(fimTime);
+        }
+    }
+}
